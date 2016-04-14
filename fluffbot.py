6442746ab8af26121a8ed73dcbebac472d509b38
Alexander <alex92_persson@hotmail.com>
2016-04-14 22:02:11 +0200

    Added support for channel join/disconnect and audio playback

--- fluffbot.py
+++ fluffbot.py
@@ -4,27 +4,84 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-client = discord.Client()
+if not discord.opus.is_loaded():
+	discord.opus.load_opus(r"C:\Users\Alexander\git\FluffBot\lib\opus\libopus-0.dll")
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+class Bot(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.player  = None
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+    def is_playing(self):
+        return self.player is not None and self.player.is_playing()
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
 
-client.run('token')
+        if message.channel.is_private:
+            await self.send_message(message.channel, 'You cannot use this bot in private messages.')
+
+        elif message.content.startswith('!join'):
+            channel_name = message.content[5:].strip()
+            check = lambda c: c.name == channel_name and c.type == discord.ChannelType.voice
+            channel = discord.utils.find(check, message.server.channels)
+            if channel is None:
+                await self.send_message(message.channel, 'Cannot find a voice channel by that name.')
+            else:
+                if self.is_voice_connected():
+                	await self.voice.disconnect()
+                await self.join_voice_channel(channel)
+
+        elif message.content.startswith('!leave'):
+            if self.is_voice_connected():
+            	await self.voice.disconnect()
+
+        elif message.content.startswith('!tadetlugnt'):
+        	if self.player is not None and self.player.is_playing():
+        		return
+
+        	if not self.is_voice_connected():
+                    await self.send_message(message.channel, 'Not connected to a voice channel.')
+                    return
+
+        	self.player = self.voice.create_ffmpeg_player(r'audio\ta_det_lugnt.mp3')
+        	self.player.start()
+
+        elif message.content.startswith('!brainpower'):
+        	if self.player is not None and self.player.is_playing():
+        		return
+
+        	if not self.is_voice_connected():
+                    await self.send_message(message.channel, 'Not connected to a voice channel.')
+                    return
+
+        	self.player = self.voice.create_ffmpeg_player(r'audio\brain_power_adj.mp3')
+        	self.player.start()
+
+        elif message.content.startswith('!gustavpower'):
+        	if self.player is not None and self.player.is_playing():
+        		return
+
+        	if not self.is_voice_connected():
+                    await self.send_message(message.channel, 'Not connected to a voice channel.')
+                    return
+
+        	self.player = self.voice.create_ffmpeg_player(r'audio\gustav_power.mp3')
+        	self.player.start()
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+
+email, password = '', ''
+
+with open(r'input\token.txt', 'r') as token:
+	email = token.readline().replace('\n', '')
+	password = token.readline().replace('\n', '')
+
+bot = Bot()
+bot.run(email, password)
