01e55945b3b991a2cedaa98227d4d523bfef5d9b
Alexander Persson <alexander.persson@ericsson.com>
2016-07-29 14:24:36 +0200

    Various improvements:
    * Adapted to breaking changes in discord.py
    * Added ?help command to display valid bot commands
    * Added bot auto-join-channel functionality

--- fluffbot.py
+++ fluffbot.py
@@ -26,6 +26,7 @@ class Bot(discord.Client):
         super().__init__()
         self.player  = None
         self.streams = self.init_streams()
+        self.server = list(self.servers)[0]
 
     def is_playing(self):
         return self.player is not None and self.player.is_playing()
@@ -50,23 +51,27 @@ class Bot(discord.Client):
             if channel is None:
                 await self.send_message(message.channel, 'Cannot find a voice channel by that name.')
             else:
-                if self.is_voice_connected():
+                if self.is_voice_connected(self.server):
                 	await self.voice.disconnect()
                 await self.join_voice_channel(channel)
 
         elif message.content.startswith('$leave'):
-            if self.is_voice_connected():
+            if self.is_voice_connected(self.server):
             	await self.voice.disconnect()
 
         elif message.content.startswith('!'):
         	if self.player is not None and self.player.is_playing():
         		return
 
-        	if not self.is_voice_connected():
+        	if not self.is_voice_connected(self.server):
         		await self.send_message(message.channel, 'Not connected to a voice channel.')
         		return
 
-        	command = message.content[1:len(message.content)]
+        	if not message.author.voice.voice_channel == bot.voice.voice_channel:
+                await.self.send_message(message.channel, 'You need to be in this voice channel to play audio.')
+                return
+
+            command = message.content[1:len(message.content)]
         	with open(r'input\audio.txt', 'r') as audio_handle:
         		s = audio_handle.read()
         		dic = ast.literal_eval(s)
@@ -74,7 +79,17 @@ class Bot(discord.Client):
         			self.player = self.voice.create_ffmpeg_player('audio\\' + dic[command])
         			self.player.start()
         		else:
-        			await self.send_message(message.channel, 'No such command.')
+        			await self.send_message(message.channel, 'No such command. Type ?help for a list of commands.')
+
+        elif message.content.startswith('?help'):
+            help_string = 'Bot commands: '
+            with open(r'input\audio.txt', 'r') as audio_handle:
+                s = audio_handle.read()
+                dic = ast.literal_eval(s)
+                for command in dic.keys()
+                    help_string += '!' + command + ', '
+                help_string = help_string[:len(help_string) - 2]
+                await self.send_message(message.channel, help_string)
 
     async def on_ready(self):
         print('Logged in as')
@@ -84,7 +99,7 @@ class Bot(discord.Client):
 
 async def poll_twitch():
 	await bot.wait_until_ready()
-	channel = discord.Object(id=discord.utils.find(lambda m: m.name == 'general', list(bot.servers)[0].channels).id)
+	channel = discord.Object(id=discord.utils.find(lambda m: m.name == 'general', bot.server.channels).id)
 	while not bot.is_closed:
 		for user, online in bot.streams.items():
 			try:
@@ -101,16 +116,27 @@ async def poll_twitch():
 			
 		await asyncio.sleep(60)
 
-email, password = '', ''
+async def auto_join():
+    await bot.wait_until_ready()
+    voice_channels = list(filter(lambda c: c.type == ChannelType.voice, bot.server.channels))
+    voice_channels.sort(key=lambda vc: len(vc.voice_members))
+    candidate = voice_channels.pop()
+    if (not bot.is_voice_connected(bot.server) or not 
+            len(list(filter(lambda vc: len(vc.voice_members) 
+            == len(candidate.voice_members), voice_channels))) > 0):
+        await bot.join_voice_channel(candidate)
+    asyncio.sleep(10)
+
+login_token = ''
 
 with open(r'input\token.txt', 'r') as token:
-	email = token.readline().replace('\n', '')
-	password = token.readline().replace('\n', '')
+	login_token = token.readline().replace('\n', '')
 
 bot = Bot()
 try:
-	bot.loop.create_task(poll_twitch())
-	bot.loop.run_until_complete(bot.start(email, password))
+	bot.loop.create_task(auto_join())
+    bot.loop.create_task(poll_twitch())
+	bot.loop.run_until_complete(bot.start(login_token))
 except KeyboardInterrupt:
 	bot.loop.run_until_complete(bot.logout())
 	pending = asyncio.Task.all_tasks()
