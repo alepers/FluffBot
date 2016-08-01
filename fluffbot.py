f0ec6f20bdb0be0552ae1dba9fb6bda4c2f35236
Alexander Persson <alex92_persson@hotmail.com>
2016-08-01 02:33:14 +0200

    Minor edits

--- fluffbot.py
+++ fluffbot.py
@@ -10,7 +10,7 @@ import json
 logging.basicConfig(level=logging.INFO)
 
 if not discord.opus.is_loaded():
-    discord.opus.load_opus(r"C:\Users\Alexander\git\FluffBot\lib\opus\libopus-0.dll")
+    discord.opus.load_opus(r"C:\Users\Alexander\FluffBot\lib\opus\libopus-0.x64.dll")
 
 class Bot(discord.Client):
     def init_streams(self):
@@ -26,7 +26,7 @@ class Bot(discord.Client):
         super().__init__()
         self.player  = None
         self.streams = self.init_streams()
-        self.server = list(self.servers)[0]
+        self.server = None
 
     def is_playing(self):
         return self.player is not None and self.player.is_playing()
@@ -36,6 +36,13 @@ class Bot(discord.Client):
 
     def go_offline(self, user):
         self.streams[user] = False
+
+    def get_server(self):
+        if self.server is not None:
+            return self.server
+        else:
+            print("Server is null!")
+            return None
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -52,12 +59,12 @@ class Bot(discord.Client):
                 await self.send_message(message.channel, 'Cannot find a voice channel by that name.')
             else:
                 if self.is_voice_connected(self.server):
-                    await self.voice.disconnect()
+                    await self.voice_client_in(self.server).disconnect()
                 await self.join_voice_channel(channel)
 
         elif message.content.startswith('$leave'):
             if self.is_voice_connected(self.server):
-                await self.voice.disconnect()
+                await self.voice_client_in(self.server).disconnect()
 
         elif message.content.startswith('!'):
             if self.player is not None and self.player.is_playing():
@@ -67,8 +74,8 @@ class Bot(discord.Client):
                 await self.send_message(message.channel, 'Not connected to a voice channel.')
                 return
 
-            if not message.author.voice.voice_channel == bot.voice.voice_channel:
-                await.self.send_message(message.channel, 'You need to be in this voice channel to play audio.')
+            if not message.author.voice.voice_channel == self.voice_client_in(self.server).channel:
+                await self.send_message(message.channel, 'You need to be in this voice channel to play audio.')
                 return
 
             command = message.content[1:len(message.content)]
@@ -76,7 +83,7 @@ class Bot(discord.Client):
                 s = audio_handle.read()
                 dic = ast.literal_eval(s)
                 if command in dic:
-                    self.player = self.voice.create_ffmpeg_player('audio\\' + dic[command])
+                    self.player = self.voice_client_in(self.server).create_ffmpeg_player('audio\\' + dic[command])
                     self.player.start()
                 else:
                     await self.send_message(message.channel, 'No such command. Type ?help for a list of commands.')
@@ -86,20 +93,21 @@ class Bot(discord.Client):
             with open(r'input\audio.txt', 'r') as audio_handle:
                 s = audio_handle.read()
                 dic = ast.literal_eval(s)
-                for command in dic.keys()
+                for command in dic.keys():
                     help_string += '!' + command + ', '
                 help_string = help_string[:len(help_string) - 2]
                 await self.send_message(message.channel, help_string)
 
     async def on_ready(self):
+        self.server = list(self.servers)[0]
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
 
 async def poll_twitch():
-    await bot.wait_until_ready()
-    channel = discord.Object(id=discord.utils.find(lambda m: m.name == 'general', bot.server.channels).id)
+    await asyncio.sleep(10)
+    channel = discord.Object(id=discord.utils.find(lambda m: m.name == 'general', bot.get_server().channels).id)
     while not bot.is_closed:
         for user, online in bot.streams.items():
             try:
@@ -113,20 +121,23 @@ async def poll_twitch():
                         await bot.send_message(channel, user + ' just started streaming! https://www.twitch.tv/' + user)
             except:
                 pass
-            
+
         await asyncio.sleep(60)
 
 async def auto_join():
-    await bot.wait_until_ready()
-    voice_channels = list(filter(lambda c: c.type == ChannelType.voice, bot.server.channels))
-    voice_channels.sort(key=lambda vc: len(vc.voice_members))
-    candidate = voice_channels.pop()
-    if (not bot.is_voice_connected(bot.server) or not 
-            len(list(filter(lambda vc: len(vc.voice_members) 
-            == len(candidate.voice_members), voice_channels))) > 0):
-        await bot.join_voice_channel(candidate)
-    
     await asyncio.sleep(10)
+    while not bot.is_closed:
+        voice_channels = list(filter(lambda c: c.type == discord.ChannelType.voice and c != bot.get_server().afk_channel, bot.get_server().channels))
+        voice_channels.sort(key=lambda vc: len(vc.voice_members) - 1 if bot.user in vc.voice_members else len(vc.voice_members))
+        candidate = voice_channels.pop()
+        if not bot.is_voice_connected(bot.get_server()): 
+            await bot.join_voice_channel(candidate)
+        else:
+            if not len(list(filter(lambda vc: len(vc.voice_members) 
+                    == len(candidate.voice_members), voice_channels))) > 0:
+                await bot.voice_client_in(bot.get_server()).move_to(candidate)
+        
+        await asyncio.sleep(10)
 
 login_token = ''
 
