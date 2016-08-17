83385aaa2b27a094d0b1eefab10ae3d439b5ade8
Alexander Persson <alex92_persson@hotmail.com>
2016-08-17 12:51:53 +0200

    Add check util with bot owner check

---
+++ cogs/utils/checks.py
@@ -0,0 +1,8 @@
+from discord.ext import commands
+import discord.utils
+
+def is_owner_check(message):
+    return message.author.id == '140093153922252800'
+
+def is_owner():
+    return commands.check(lambda ctx: is_owner_check(ctx.message))
