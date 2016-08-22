b71a4623c439316c80f6129305283348af8760a8
Alexander Persson <alex92_persson@hotmail.com>
2016-08-22 10:59:57 +0200

    Start work on commands port

--- fluffbot_ext.py
+++ fluffbot_ext.py
@@ -1,6 +1,36 @@
 from discord.ext import commands
 import discord
+from cogs.utils import checks
+import logging
 
 description = """
 A Discord bot written by alepers.
 """
+
+logging.basicConfig(level=logging.INFO)
+
+if not discord.opus.is_loaded():
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    opus_path = os.path.join(script_path, 'lib', 'opus', 'libopus-0.x64.dll')
+    discord.opus.load_opus(opus_path)
+
+prefix = ['!', '?']
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print('Username: ' + bot.user.name)
+    print('ID: ' + bot.user.id)
+    print('------')
+
+@bot.event
+async def on_resumed():
+    print('resumed...')
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await bot.process_commands(message)
