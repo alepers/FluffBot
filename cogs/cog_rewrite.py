9a66ef3160cbc954785e186307c7e9aa33065bc4
Alexander Persson <alex92_persson@hotmail.com>
2019-01-28 09:50:32 +0100

    Migrate to discord.py v1.0 (rewrite)

---
+++ cogs/cog_rewrite.py
@@ -0,0 +1,103 @@
+from discord.ext import commands
+from .utils import checks
+import discord
+import asyncio
+import inspect
+import datetime
+import random
+import ast
+import os
+
+
+class BotCommands:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(hidden=True)
+    @checks.is_owner()
+    async def debug(self, ctx, *, code : str):
+        """Lets the bot owner evaluate arbitrary code."""
+
+        code = code.strip('` ')
+        python = '```py\n{}\n```'
+        result = None
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'message': ctx.message,
+            'server': ctx.message.server,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author
+        }
+
+        env.update(globals())
+
+        try:
+            result = eval(code, env)
+            if inspect.isawaitable(result):
+                result = await result
+        except Exception as e:
+            await ctx.send(python.format(type(e).__name__ + ': ' + str(e)))
+            return
+
+        await ctx.send(python.format(result))
+
+
+    @commands.command()
+    async def roll(self, ctx):
+        """Rolls for a number between 0-100."""
+
+        minimum = 0
+        maximum = 100
+
+        await ctx.send('{} rolls {}.'.format(ctx.author.mention, random.randint(minimum, maximum)))
+
+
+    @commands.command()
+    async def play(self, ctx, *, audio : str):
+        """Plays a specified sound clip. Use 'play help' to see a list of clips."""
+
+        bot_vc = discord.utils.find(lambda vc: vc.guild == ctx.guild, self.bot.voice_clients)
+        if not bot_vc or not bot_vc.is_connected():
+            await ctx.send('I am not connected to a voice channel yet, hold on..')
+            return
+
+        if bot_vc.is_playing():
+            return
+
+        if ctx.author.voice:
+            if not ctx.author.voice.channel == bot_vc.channel:
+                await ctx.send('You need to be in this voice channel to play audio.')
+                return
+
+        audio_lowercase = audio.lower()
+        with open(os.path.join('input', 'audio.txt'), 'r') as audio_fd:
+            content = audio_fd.read()
+            audio_dict = ast.literal_eval(content)
+            if audio_lowercase == 'help':
+                await ctx.send('Audio clips: `' + ', '.join(sorted(audio_dict.keys())) + '`')
+                return
+
+            if audio_lowercase in audio_dict:
+                bot_vc.play(discord.FFmpegPCMAudio(os.path.join('audio', audio_dict[audio_lowercase])))
+
+                # Clean up play command message
+                await asyncio.sleep(2)
+                await ctx.message.delete()
+            else:
+                await ctx.send('No such audio clip. Type `!play help` for a list of clips.')
+
+
+    @commands.command()
+    @commands.cooldown(1, 60.0, type=commands.BucketType.channel)
+    async def source(self, ctx):
+        """Displays a link to the FluffBot source code on GitHub."""
+
+        await ctx.send('https://github.com/alepers/FluffBot')
+
+
+def setup(bot):
+    bot.add_cog(BotCommands(bot))
---
+++ fluffbot_rewrite.py
@@ -0,0 +1,149 @@
+from discord.ext import commands
+import discord
+from cogs.utils import checks
+import logging
+import json
+import os
+import aiohttp
+import asyncio
+
+description = """
+A Discord bot written by alepers.
+"""
+extensions = [
+    'cogs.cog_rewrite'
+]
+
+logging.basicConfig(level=logging.INFO)
+
+if not discord.opus.is_loaded():
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    opus_path = os.path.join(script_path, 'lib', 'opus', 'libopus-0.x64.dll')
+    discord.opus.load_opus(opus_path)
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!', '?'), 
+                   description=description,
+                   case_insensitive=True)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print('Username: ' + bot.user.name)
+    print('ID: ' + str(bot.user.id))
+    print('------')
+
+
+@bot.event
+async def on_resumed():
+    print('resumed...')
+
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await bot.process_commands(message)
+
+
+def load_credentials():
+    with open('credentials.json') as f:
+        return json.load(f)
+
+
+def init_streams():
+    with open(os.path.join('input', 'streams.txt'), 'r') as file:
+        stream_names = file.readlines()
+        stream_names = [x.strip('\n') for x in stream_names]
+        stream_statuses = []
+        for stream_name in stream_names:
+            stream_statuses.append((stream_name, False))
+        return dict(stream_statuses)
+
+
+async def poll_twitch():
+    await asyncio.sleep(20)
+
+    if not hasattr(bot, 'streams'):
+        bot.streams = init_streams()
+
+    while not bot.is_closed():
+        for guild in bot.guilds:
+            channel = discord.utils.find(lambda c: c.name == "general", guild.channels)
+
+            if channel:
+                for user, online in bot.streams.items():
+                    try:
+                        url = 'https://api.twitch.tv/kraken/streams/' + user
+                        headers = {'Client-Id': twitch_client_id}
+                        info = ''
+
+                        async with aiohttp.ClientSession() as session:
+                            async with session.get(url, headers=headers) as response:
+                                info = await response.json()
+
+                        if info['stream'] == None:
+                            if online:
+                                bot.streams[user] = False
+                        else:
+                            if not online:
+                                bot.streams[user] = True
+                                await channel.send(user + ' just started streaming! https://www.twitch.tv/' + user)
+                    except:
+                        pass
+
+        await asyncio.sleep(60)
+
+
+async def auto_join():
+    await asyncio.sleep(15)
+    while not bot.is_closed():
+        for guild in bot.guilds:
+            voice_channels = list(filter(lambda c: isinstance(c, discord.VoiceChannel)
+                                                   and c != guild.afk_channel
+                                                   and not bot.user in c.members,
+                                         guild.channels))
+            voice_channels.sort(key=lambda c: len(c.members))
+            candidate = voice_channels.pop()
+            connected_client = discord.utils.find(lambda vc: vc.guild == guild, bot.voice_clients)
+            if not connected_client:
+                await candidate.connect()
+            else:
+                if connected_client.channel != candidate:
+                    if len(connected_client.channel.members) - 1 < len(candidate.members):
+                        await connected_client.move_to(candidate)
+
+        await asyncio.sleep(10)
+
+
+def main():
+    credentials = load_credentials()
+    twitch_client_id = credentials['twitch_client_id']
+
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
+
+    try:
+        bot.loop.create_task(auto_join())
+        bot.loop.create_task(poll_twitch())
+        bot.loop.run_until_complete(bot.start(credentials['token']))
+    except KeyboardInterrupt:
+        bot.loop.run_until_complete(bot.logout())
+        pending = asyncio.Task.all_tasks()
+        gathered = asyncio.gather(*pending)
+        try:
+            gathered.cancel()
+            bot.loop.run_forever()
+            gathered.exception()
+        except:
+            pass
+    finally:
+        bot.loop.close()
+
+
+if __name__ == '__main__':
+    main()
